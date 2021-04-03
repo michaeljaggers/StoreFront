5fe104de4509a7ab3efcbf3cd7ff3dcbf756d51b
Michael Jaggers <michael.jaggers@gmail.com.com>
2021-04-02 21:06:01 -0500

    Removed unneeded CSS and JS files.  Update bundles for some JS and styles in BundleConfig.cs

--- StoreFront.UI/App_Start/BundleConfig.cs
+++ StoreFront.UI/App_Start/BundleConfig.cs
@@ -8,10 +8,16 @@ namespace StoreFront.UI
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Content/vendor/jquery/jquery.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/lightbox").Include(
+                        "~/Content/vendor/lightbox2/js/lightbox.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
+                        "~/Scripts/js/front.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
@@ -19,12 +25,12 @@ namespace StoreFront.UI
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                        "~/Content/vendor/bootstrap/js/bootstrap.min.js",
+                        "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                        "~/Content/css/style.default.min.css",
+                        "~/Content/css/custom.css"));
         }
     }
 }
