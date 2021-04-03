994df3c12ea5847fbde91cc4ccc07012765cfa2e
Michael Jaggers <michael.jaggers@gmail.com.com>
2021-04-02 21:28:01 -0500

    Added bundles to BundleConfig for vendor scripts.  Linked bundles in _Layout.cshtml

--- StoreFront.UI/App_Start/BundleConfig.cs
+++ StoreFront.UI/App_Start/BundleConfig.cs
@@ -16,8 +16,18 @@ namespace StoreFront.UI
             bundles.Add(new ScriptBundle("~/bundles/lightbox").Include(
                         "~/Content/vendor/lightbox2/js/lightbox.min.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
+            bundles.Add(new ScriptBundle("~/bundles/front").Include(
                         "~/Scripts/js/front.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/nouislider").Include(
+                        "~/Content/vendor/nouislider/nouislider.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap-select").Include(
+                        "~/Content/vendor/bootstrap-select/js/bootstrap-select.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/owl-carousel2").Include(
+                        "~/Content/vendor/owl.carousel2/owl.carousel.min.js",
+                        "~/Content/vendor/owl.carousel2.thumbs/owl.carousel2.thumbs.min.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
