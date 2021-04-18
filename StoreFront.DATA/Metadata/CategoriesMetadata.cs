6362ad4e07d01f3a071840d17e3062d0ff294017
Michael Jaggers <michael.jaggers@gmail.com.com>
2021-04-18 15:57:32 -0500

    Started annotating EF objects in buddy classes.

---
+++ StoreFront.DATA/Metadata/CategoriesMetadata.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreFront
+{
+    public class CategoriesMetadata
+    {
+        [ScaffoldColumn(false)]
+        public int CategoryID { get; set; }
+
+        [Required]
+        [Display(Name = "Category")]
+        [StringLength(30, ErrorMessage = "Maximum length is 30 characters.")]
+        public string Name { get; set; }
+
+        public string Image { get; set; }
+
+        public string Description { get; set; }
+    }
+
+    [MetadataType(typeof(CategoriesMetadata))]
+    public partial class Categories { }
+}
---
+++ StoreFront.DATA/Metadata/CustomerMetadata.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreFront.DATA.Metadata
+{
+    public class CustomerMetadata
+    {
+        [ScaffoldColumn(false)]
+        public int CustomerID { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        [StringLength(30, ErrorMessage = "Maximum length is 30 characters.")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        [StringLength(30, ErrorMessage = "Maximum length is 30 characters.")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Street Address")]
+        [StringLength(60, ErrorMessage = "Maximum length is 60 characters.")]
+        public string Address { get; set; }
+
+        [StringLength(20, ErrorMessage = "Maximum length is 20 characters.")]
+        public string City { get; set; }
+
+        [StringLength(15, ErrorMessage = "Maximum length is 15 characters.")]
+        public string State { get; set; }
+
+        [Display(Name = "Zip Code")]
+        [StringLength(10, ErrorMessage = "Maximum length is 10 characters.")]
+        public string Zip { get; set; }
+
+        [StringLength(15, ErrorMessage = "Maximum length is 15 characters.")]
+        public string Country { get; set; }
+
+        [EmailAddress]
+        [StringLength(30, ErrorMessage = "Maximum length is 30 characters.")]
+        public string Email { get; set; }
+
+        [StringLength(24, ErrorMessage = "Maximum length is 24 characters.")]
+        public string Phone { get; set; }
+    }
+
+    [MetadataType(typeof(CustomerMetadata)]
+    public partial class Customer { }
+}
