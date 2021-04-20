f907dea8235ace447db3abb4f6920ea8f7cb0238
Michael Jaggers <michael.jaggers@gmail.com.com>
2021-04-20 16:26:32 -0500

    Start configuring contact form

--- StoreFront.UI/Controllers/HomeController.cs
+++ StoreFront.UI/Controllers/HomeController.cs
@@ -1,4 +1,9 @@
-﻿using System.Web.Mvc;
+﻿using StoreFront.UI.Models;
+using System;
+using System.Net;
+using System.Net.Mail;
+using System.Web.Mvc;
+
 
 namespace StoreFront.UI.Controllers
 {
@@ -21,6 +26,40 @@ namespace StoreFront.UI.Controllers
         public ActionResult Contact()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Contact(ContactViewModel cvm)
+        {
+            if (ModelState.IsValid)
+            {
+                string message = $"You have received a new contact form submisson.<br /><br />" +
+                                 $"Name: {cvm.Name}<br />" +
+                                 $"Email: {cvm.Email}<br />" +
+                                 $"Message:<br /><br />" +
+                                 $"{cvm.Message}";
+
+                MailMessage mm = new MailMessage("admin@michaeljaggers.net", "michaeljaggers@outlook.com", "New contact form submission.", message);
+                mm.IsBodyHtml = true;
+                mm.ReplyToList.Add(cvm.Email);
+                mm.Priority = MailPriority.High;
+
+                SmtpClient client = new SmtpClient("mail.michaeljaggers.net");
+                client.Credentials = new NetworkCredential("admin@michaeljaggers.net", "****");
+
+                try
+                {
+                    client.Send(mm);
+                }
+                catch (Exception e)
+                {
+                    ViewBag.ErrorMessage = $"Your message could not be sent at this time.  Error: {e.Message}";
+                }
+
+                return View("EmailConfirmation");
+            }
+
+            return View(cvm);
         }
 
         [HttpGet]
---
+++ StoreFront.UI/Models/ContactViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace StoreFront.UI.Models
+{
+    public class ContactViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [UIHint("MultilineText")]
+        public string Message { get; set; }
+    }
+}
