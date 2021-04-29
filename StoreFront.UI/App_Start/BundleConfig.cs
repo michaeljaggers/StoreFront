d9427cab8f8b53ab5041b4433a97eac723bd7e6f
Michael Jaggers <michael.jaggers@gmail.com.com>
2021-04-28 22:04:14 -0500

    Re-do bundles for performance edit styles

--- StoreFront.UI/App_Start/BundleConfig.cs
+++ StoreFront.UI/App_Start/BundleConfig.cs
@@ -7,34 +7,26 @@ namespace StoreFront.UI
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Content/vendor/jquery/jquery.min.js"));
 
-            bundles.Add(new ScriptBundle("~/Scripts/jquery.validate*").Include(
-                        "~/Scripts/jquery.validate*"));
+            // Script Bundles
 
-            bundles.Add(new ScriptBundle("~/bundles/lightbox").Include(
-                        "~/Content/vendor/lightbox2/js/lightbox.min.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/front").Include(
+            bundles.Add(new ScriptBundle("~/bundles/js").Include(
+                        "~/Content/vendor/jquery/jquery.min.js",
+                        "~/Scripts/DataTables/jquery.dataTables.min.js",
+                        "~/Content/vendor/bootstrap/js/bootstrap.min.js",
+                        "~/Scripts/respond.js",
+                        "~/Content/vendor/bootstrap-select/js/bootstrap-select.min.js",
+                        "~/Content/vendor/lightbox2/js/lightbox.min.js",
+                        "~/Content/vendor/nouislider/nouislider.min.js",
+                        "~/Content/vendor/owl.carousel2/owl.carousel.min.js",
+                        "~/Content/vendor/owl.carousel2.thumbs/owl.carousel2.thumbs.min.js",
                         "~/Scripts/js/front.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/nouislider").Include(
-                        "~/Content/vendor/nouislider/nouislider.min.js"));
+            bundles.Add(new ScriptBundle("~/bundles/contact").Include(
+                        "~/Scripts/jquery.unobtrusive-ajax.min.js",
+                        "~/Scripts/jquery.validate*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap-select").Include(
-                        "~/Content/vendor/bootstrap-select/js/bootstrap-select.min.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/owl-carousel2").Include(
-                        "~/Content/vendor/owl.carousel2/owl.carousel.min.js",
-                        "~/Content/vendor/owl.carousel2.thumbs/owl.carousel2.thumbs.min.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                        "~/Content/vendor/bootstrap/js/bootstrap.min.js",
-                        "~/Scripts/respond.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/data-tables").Include(
-                        "~/Scripts/DataTables/jquery.dataTables.min.js"));
+            // Style Bundles
 
             bundles.Add(new StyleBundle("~/Content/vendor").Include(
                         "~/Content/vendor/bootstrap/css/bootstrap.min.css",
@@ -43,15 +35,10 @@ namespace StoreFront.UI
                         "~/Content/vendor/nouislider/nouislider.min.css",
                         "~/Content/vendor/owl.carousel2/assets/owl.carousel.min.css",
                         "~/Content/vendor/owl.carousel2/assets/owl.theme.default.min.css",
-                        "~/Content/DataTables/css/jquery.dataTables.jqueryui.min.css"));
-
-            bundles.Add(new ScriptBundle("~/bundles/contact").Include(
-                        "~/Scripts/jquery.unobtrusive-ajax.min.js",
-                        "~/Scripts/jquery.validate*"));
-
+                        "~/Content/DataTables/css/jquery.dataTables.jqueryui.min.css",
+                        "~/Content/DataTables/css/jquery.dataTables.min.css"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                        "~/Content/DataTables/css/jquery.dataTables.min.css",
                         "~/Content/css/style.default.min.css",
                         "~/Content/css/custom.css"));
         }
