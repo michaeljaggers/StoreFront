ee3f547b45182a2b213fd421dec71b3384a057d9
Michael Jaggers <michael.jaggers@gmail.com.com>
2021-05-02 13:30:34 -0500

    Finished add/edit/delete image functionality for Categories

--- StoreFront.UI/Controllers/CategoriesController.cs
+++ StoreFront.UI/Controllers/CategoriesController.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
+using System.Drawing;
 using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using StoreFront.DATA;
+using StoreFront.UI.Utilities;
 
 namespace StoreFront.UI.Controllers
 {
@@ -46,10 +48,52 @@ namespace StoreFront.UI.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "CategoryID,Name,Image,Description")] Category category)
+        public ActionResult Create([Bind(Include = "CategoryID,Name,Image,Description")] Category category, HttpPostedFileBase categoryImage)
         {
             if (ModelState.IsValid)
             {
+
+                #region File Upload
+                // Default image if none provided
+                string file = "noImage.png";
+
+                // Check if user uploaded an image
+                if (categoryImage != null)
+                {
+                    // Preserve the file name for the image
+                    file = categoryImage.FileName;
+
+                    // Isolate the extension
+                    string ext = file.Substring(file.LastIndexOf('.'));
+
+                    // Create an array of good extensions
+                    string[] goodExts = { ".jpeg", ".jpg", ".png", ".gif" };
+
+                    //Check if the uploaded file extension is in our list of good extensions & check that the file size is <= 4MB max imposed by ASP.net
+                    if (goodExts.Contains(ext.ToLower()) && categoryImage.ContentLength <= 4194304)
+                    {
+                        // Create a new file name (using a GUID)
+                        file = Guid.NewGuid() + ext;
+
+                        #region Resize Image
+                        string savePath = Server.MapPath("~/Content/imgstore/categories/");
+
+                        Image convertedImage = Image.FromStream(categoryImage.InputStream);
+
+                        int maxImageSize = 500;
+
+                        int maxThumbSize = 100;
+
+                        ImageUtility.ResizeImage(savePath, file, convertedImage, maxImageSize, maxThumbSize);
+                        #endregion
+                    }
+                }
+
+                // No matter what, update the image url with the value of the file variable
+                category.Image = file;
+
+                #endregion
+
                 db.Categories.Add(category);
                 db.SaveChanges();
                 return RedirectToAction("Index");
@@ -78,10 +122,57 @@ namespace StoreFront.UI.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "CategoryID,Name,Image,Description")] Category category)
+        public ActionResult Edit([Bind(Include = "CategoryID,Name,Image,Description")] Category category, HttpPostedFileBase categoryImage)
         {
             if (ModelState.IsValid)
             {
+
+                #region File Edit
+                // Default image if none provided
+                string file = "noImage.png";
+
+                // Check if user uploaded an image
+                if (categoryImage != null)
+                {
+                    // Preserve the file name for the image
+                    file = categoryImage.FileName;
+
+                    // Isolate the extension
+                    string ext = file.Substring(file.LastIndexOf('.'));
+
+                    // Create an array of good extensions
+                    string[] goodExts = { ".jpeg", ".jpg", ".png", ".gif" };
+
+                    //Check if the uploaded file extension is in our list of good extensions & check that the file size is <= 4MB max imposed by ASP.net
+                    if (goodExts.Contains(ext.ToLower()) && categoryImage.ContentLength <= 4194304)
+                    {
+                        // Create a new file name (using a GUID)
+                        file = Guid.NewGuid() + ext;
+
+                        #region Resize Image
+                        string savePath = Server.MapPath("~/Content/imgstore/categories/");
+
+                        Image convertedImage = Image.FromStream(categoryImage.InputStream);
+
+                        int maxImageSize = 500;
+
+                        int maxThumbSize = 100;
+
+                        ImageUtility.ResizeImage(savePath, file, convertedImage, maxImageSize, maxThumbSize);
+                        #endregion
+
+                        if (category.Image != null && category.Image != "noImage.png")
+                        {
+                            string path = Server.MapPath("~/Content/imgstore/categories/");
+                            ImageUtility.Delete(path, category.Image);
+                        }
+                    }
+
+                    // No matter what, update the image url with the value of the file variable
+                    category.Image = file;
+                }
+                #endregion
+
                 db.Entry(category).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
@@ -110,6 +201,10 @@ namespace StoreFront.UI.Controllers
         public ActionResult DeleteConfirmed(int id)
         {
             Category category = db.Categories.Find(id);
+
+            string path = Server.MapPath("~/Content/imgstore/categories/");
+            ImageUtility.Delete(path, category.Image);
+
             db.Categories.Remove(category);
             db.SaveChanges();
             return RedirectToAction("Index");
