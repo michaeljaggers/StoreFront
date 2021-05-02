7ff6ae61944c0d57482b89c11a7fd2e4698386ab
Michael Jaggers <michael.jaggers@gmail.com.com>
2021-05-02 15:54:14 -0500

    Add create/edit/delete functionailty for flavor images

--- StoreFront.UI/Controllers/FlavorsController.cs
+++ StoreFront.UI/Controllers/FlavorsController.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
+using System.Drawing;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -47,10 +49,52 @@ namespace StoreFront.UI.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "FlavorID,Name,Image,Description")] Flavor flavor)
+        public ActionResult Create([Bind(Include = "FlavorID,Name,Image,Description")] Flavor flavor, HttpPostedFileBase flavorImage)
         {
             if (ModelState.IsValid)
             {
+
+                #region File Upload
+                // Default image if none provided
+                string file = "noImage.png";
+
+                // Check if user uploaded an image
+                if (flavorImage != null)
+                {
+                    // Preserve the file name for the image
+                    file = flavorImage.FileName;
+
+                    // Isolate the extension
+                    string ext = file.Substring(file.LastIndexOf('.'));
+
+                    // Create an array of good extensions
+                    string[] goodExts = { ".jpeg", ".jpg", ".png", ".gif" };
+
+                    //Check if the uploaded file extension is in our list of good extensions & check that the file size is <= 4MB max imposed by ASP.net
+                    if (goodExts.Contains(ext.ToLower()) && flavorImage.ContentLength <= 4194304)
+                    {
+                        // Create a new file name (using a GUID)
+                        file = Guid.NewGuid() + ext;
+
+                        #region Resize Image
+                        string savePath = Server.MapPath("~/Content/imgstore/flavors/");
+
+                        Image convertedImage = Image.FromStream(flavorImage.InputStream);
+
+                        int maxImageSize = 500;
+
+                        int maxThumbSize = 100;
+
+                        ImageUtility.ResizeImage(savePath, file, convertedImage, maxImageSize, maxThumbSize);
+                        #endregion
+                    }
+                }
+
+                // No matter what, update the image url with the value of the file variable
+                flavor.Image = file;
+
+                #endregion
+
                 db.Flavors.Add(flavor);
                 db.SaveChanges();
                 return RedirectToAction("Index");
@@ -79,10 +123,57 @@ namespace StoreFront.UI.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "FlavorID,Name,Image,Description")] Flavor flavor)
+        public ActionResult Edit([Bind(Include = "FlavorID,Name,Image,Description")] Flavor flavor, HttpPostedFileBase flavorImage)
         {
             if (ModelState.IsValid)
             {
+
+                #region File Edit
+                // Default image if none provided
+                string file = "noImage.png";
+
+                // Check if user uploaded an image
+                if (flavorImage != null)
+                {
+                    // Preserve the file name for the image
+                    file = flavorImage.FileName;
+
+                    // Isolate the extension
+                    string ext = file.Substring(file.LastIndexOf('.'));
+
+                    // Create an array of good extensions
+                    string[] goodExts = { ".jpeg", ".jpg", ".png", ".gif" };
+
+                    //Check if the uploaded file extension is in our list of good extensions & check that the file size is <= 4MB max imposed by ASP.net
+                    if (goodExts.Contains(ext.ToLower()) && flavorImage.ContentLength <= 4194304)
+                    {
+                        // Create a new file name (using a GUID)
+                        file = Guid.NewGuid() + ext;
+
+                        #region Resize Image
+                        string savePath = Server.MapPath("~/Content/imgstore/flavors/");
+
+                        Image convertedImage = Image.FromStream(flavorImage.InputStream);
+
+                        int maxImageSize = 500;
+
+                        int maxThumbSize = 100;
+
+                        ImageUtility.ResizeImage(savePath, file, convertedImage, maxImageSize, maxThumbSize);
+                        #endregion
+
+                        if (flavor.Image != null && flavor.Image != "noImage.png")
+                        {
+                            string path = Server.MapPath("~/Content/imgstore/flavors/");
+                            ImageUtility.Delete(path, flavor.Image);
+                        }
+                    }
+
+                    // No matter what, update the image url with the value of the file variable
+                    flavor.Image = file;
+                }
+                #endregion
+
                 db.Entry(flavor).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
@@ -111,6 +202,9 @@ namespace StoreFront.UI.Controllers
         public ActionResult DeleteConfirmed(int id)
         {
             Flavor flavor = db.Flavors.Find(id);
+
+            string path = Server.MapPath("~/Content/imgstore/flavors/");
+            ImageUtility.Delete(path, flavor.Image);
             db.Flavors.Remove(flavor);
             db.SaveChanges();
             return RedirectToAction("Index");
